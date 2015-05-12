f646f3b0b145c7cacfcbc7aae418a76a45e54e94
dougflip <douglasdifilippo@gmail.com>
2015-05-12 12:59:02 -0400

    Examples on recursion

---
+++ ch5/recursion.hs
@@ -0,0 +1,44 @@
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "Can't max an empty list!"
+maximum' [x] = x
+maximum' (x:xs)
+  | x > maxTail = x
+  | otherwise = maxTail
+  where maxTail = maximum' xs
+
+maximumWithMax :: (Ord a) => [a] -> a
+maximumWithMax [] = error "No Max!"
+maximumWithMax [x] = x
+maximumWithMax (x:xs) = max x (maximumWithMax xs)
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+  | n <= 0 = []
+  | otherwise = x:replicate' (n-1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' 0 _ = []
+take' _ [] = []
+take' n (x:xs) = x:take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' _ [] = False
+elem' a (x:xs)
+  | a == x = True
+  | otherwise = a `elem'` xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+  let smallerSorted = quicksort [a | a <- xs, a <= x]
+      biggerSorted = quicksort [a | a <- xs, a > x]
+  in smallerSorted ++ [x] ++ biggerSorted
